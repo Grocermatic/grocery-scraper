438fcf530dd3d8c3dada367d72ec6629ce50cc45
MengLinMaker <menglinmaker@gmail.com>
2023-05-05 21:51:13 +1000

    Configure Jest code coverage 80%

--- backend/webscrape/jest.config.ts
+++ backend/webscrape/jest.config.ts
@@ -6,6 +6,18 @@ const config:Config = {
   verbose: true,
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest'
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "src/*.ts"
+  ],
+  coverageThreshold: {
+    "global": {
+      "statements": 80,
+      "branches": 80,
+      "functions": 80,
+      "lines": 80
+    }
   }
 }
 export default config;
