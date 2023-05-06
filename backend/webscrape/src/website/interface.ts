3e6063a2719c7e81bb643da1f6a3ca7f2bb59837
MengLinMaker <menglinmaker@gmail.com>
2023-05-06 20:20:30 +1000

    Add productInfo interface for data consistency

---
+++ backend/webscrape/src/website/interface.ts
@@ -0,0 +1,10 @@
+// Standardize product
+export interface ProductInfo {
+  name: string
+  url: string
+  img: string
+  price: number
+  quantity: number
+  unitPrice: number
+  nutrition: object
+}
