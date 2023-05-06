12deb4e965d5434238df9ee01b4304416b6b8e62
MengLinMaker <menglinmaker@gmail.com>
2023-05-06 15:53:41 +1000

    Scrape basic coles product info JSON

--- backend/webscrape/src/website/coles/product.ts
+++ backend/webscrape/src/website/coles/product.ts
@@ -6,8 +6,29 @@ import * as fs from 'fs'
 
 export const scrapeProductInfo = (html:string) => {
   const $ = cheerio.load(html)
+  const rawJson = JSON.parse($('script[type="application/json"]').text())
+  const rawProductJson = rawJson['props']['pageProps']['product']
+  const rawNutritionJson = rawProductJson['nutrition']['breakdown'][1]['nutrients']
+
+  console.log(rawNutritionJson)
   const productInfo = {
-    'productName': $('h1.product__title').text()
+    'name': rawProductJson['name'],
+    'url': `https://www.coles.com.au/product/${rawJson['query']['slug']}`,
+    'img': `https://productimages.coles.com.au/productimages${rawProductJson['imageUris'][0]['uri']}`,
+    'price': rawProductJson['pricing']['now'],
+    'unitPrice': rawProductJson['pricing']['unit']['price'],
+    'unitMeasure': rawProductJson['pricing']['unit']['ofMeasureUnits'],
+    'servingsPerPackage': rawProductJson['nutrition']['servingsPerPackage'],
+    'servingSize': rawProductJson['nutrition']['servingSize'],
+    'nutrition': {
+      'kilojoules': rawNutritionJson[0]['value'],
+      'protein': rawNutritionJson[1]['value'],
+      'fat': rawNutritionJson[2]['value'],
+      'carbohydrate': rawNutritionJson[3]['value'],
+      'sugars': rawNutritionJson[4]['value'],
+      'fibre': rawNutritionJson[8]['value'],
+      'sodium': rawNutritionJson[6]['value'],
+    }
   }
   return productInfo
 }
@@ -27,7 +48,7 @@ try {
   const testUrl = 'https://www.coles.com.au/product/coles-full-cream-milk-3l-8150288'
   scrapeStatic(testUrl).then((html:string) => {
     fs.writeFileSync('./src/website/product.test.html', html)
-    const $ = cheerio.load(html)
+    const productInfo = scrapeProductInfo(html)
   })
 
 }
