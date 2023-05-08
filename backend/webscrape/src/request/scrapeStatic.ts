9d133345f7dcf8bdb011f05c95fbedb4808f531e
MengLinMaker <menglinmaker@gmail.com>
2023-05-09 00:20:39 +1000

    Add proxy header and base url

--- backend/webscrape/src/request/scrapeStatic.ts
+++ backend/webscrape/src/request/scrapeStatic.ts
@@ -1,10 +1,16 @@
 import axios from 'axios'
+import { generateHtmlHeader, generatePublicIP } from './proxy'
 
 
 
 export const scrapeStatic = async(url:string):Promise<any> => {
   try {
-    const response = await axios.get(url)
+    const proxyClient = axios.create({
+      baseURL: `https://${generatePublicIP()}`,
+      timeout: 10000,
+      headers: generateHtmlHeader()['headers']
+    })
+    const response = await proxyClient.get(url, generateHtmlHeader())
     return response.data
   } catch (err:any) {
     return ''
