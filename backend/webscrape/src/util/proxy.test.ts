a4da1b551dc41663dd4da928e70dbf436ec56f81
MengLinMaker <menglinmaker@gmail.com>
2023-05-08 23:02:28 +1000

    Change user agent generation to header generation

--- backend/webscrape/src/util/proxy.test.ts
+++ backend/webscrape/src/util/proxy.test.ts
@@ -1,28 +1,28 @@
 import axios from "axios"
 
-import { generatePublicIP, generateUserAgent, userAgents } from "./proxy"
+import { generatePublicIP, generateHtmlHeader, htmlHeaders, generateRandInt } from "./proxy"
 
 
 
-describe("common phone user agent generator", () => {
-  it("should generate user agents from specified list", async()=>{
+describe("randomly pick real listed html headers", () => {
+  it("should generate from a specified list", async()=>{
     for (let mockRandom = 0; mockRandom > 1; mockRandom += 0.1) {
       jest.spyOn(global.Math, 'random').mockReturnValue(mockRandom)
-      const userAgent = generateUserAgent()
-      expect(userAgents).toContain(userAgent)
+      const userAgent = generateHtmlHeader()
+      expect(htmlHeaders).toContain(userAgent)
     }
     jest.spyOn(global.Math, 'random').mockRestore()
   })
 
   it("should alter html header", async() => {
-    const testHeader = generateUserAgent()
-    const config = {
-      headers: { 'User-Agent': testHeader }
-    }
-    const { data } = await axios.get('https://httpbin.org/headers', config)
-    const { headers } = data
-    expect(testHeader).toEqual(headers['User-Agent'])
-  })
+    const testHeaders = generateHtmlHeader() as any
+    axios.get('https://httpbin.org/headers', testHeaders).then(({ data }) => {
+      // Some custom header valuesneed to be removed
+      delete data['X-Amzn-Trace-Id']
+      delete data['Host']
+      expect(testHeaders).toEqual(data)
+    })
+  }, 30000)
 })
 
 
@@ -53,6 +53,20 @@ describe("IP generator", () => {
       expect(['VIC','NSW']).toContain(data['region'])
       expect(data['isp']).toEqual('Aussie Broadband')
     }
+    jest.spyOn(global.Math, 'random').mockRestore()
+  })
+})
+
+
+
+describe("random integer generation", () => {
+  it("should generate specified range of values", async()=>{
+    jest.spyOn(global.Math, 'random').mockReturnValue(0)
+    let randomInt = generateRandInt(10,100)
+    expect(randomInt).toEqual(10)
+    jest.spyOn(global.Math, 'random').mockReturnValue(1)
+    randomInt = generateRandInt(10,100)
+    expect(randomInt).toEqual(100)
     jest.spyOn(global.Math, 'random').mockRestore()
   })
 })
