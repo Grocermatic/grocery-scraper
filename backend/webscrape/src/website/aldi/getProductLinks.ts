6636d2f923285e3a08cb98c3157aa8be4a6fcb11
MengLinMaker <menglinmaker@gmail.com>
2023-05-09 15:32:32 +1000

    Generate product links for aldi

---
+++ backend/webscrape/src/website/aldi/getProductLinks.ts
@@ -0,0 +1,14 @@
+import { GetProductLinks } from "../interface";
+
+
+
+export const getAldiProductLinks:GetProductLinks = async() => {
+  const productLinks = [
+    'https://www.aldi.com.au/en/groceries/freezer/',
+    'https://www.aldi.com.au/en/groceries/super-savers/',
+    'https://www.aldi.com.au/en/groceries/pantry/olive-oil/',
+    'https://www.aldi.com.au/en/groceries/price-reductions/',
+    'https://www.aldi.com.au/en/groceries/fresh-produce/dairy-eggs/'
+  ]
+  return productLinks
+}
