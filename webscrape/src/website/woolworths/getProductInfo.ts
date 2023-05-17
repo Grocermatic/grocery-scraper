63840297e44ee754a9f6e8279121ddc68f97a83b
MengLinMaker <menglinmaker@gmail.com>
2023-05-17 16:32:23 +1000

    Reduce folder layers

---
+++ webscrape/src/website/woolworths/getProductInfo.ts
@@ -0,0 +1,93 @@
+import * as cheerio from 'cheerio'
+
+import { ProductInfo, ProductNutrition, GetProductInfo, GetBatchProductInfo } from "../interface"
+import { getNumFromString, roundDecimal } from "../../util/dataCleaning";
+import { scrapeDynamic } from '../../request/scrapeDynamic';
+
+
+
+export const getWoolworthsProductInfo:GetProductInfo = (html) => {
+  // Coles provides information rich JSON in script
+  const $ = cheerio.load(html)
+  const jsonString = $('script[type="application/json"]').text()
+  if (jsonString === '') return null
+
+  const rawJson = JSON.parse(jsonString)
+  const rawProductJson = rawJson.props.pageProps.product
+
+  // Calculate inaccurately provided info
+  const unitPriceCalc = getNumFromString(rawProductJson.pricing.comparable)
+  const unitPrice = roundDecimal(unitPriceCalc[0] / unitPriceCalc[1], 2)
+  const quantity = roundDecimal(rawProductJson.pricing.now / unitPrice, 2)
+
+  // Prefill mandatory values
+  const productInfo:ProductInfo = {
+    name: rawProductJson.name,
+    url: `https://www.coles.com.au/product/${rawJson.query.slug}`,
+    img: `https://productimages.coles.com.au/productimages${rawProductJson.imageUris[0].uri}`,
+    price: rawProductJson.pricing.now,
+    quantity: quantity,
+    unitPrice: unitPrice
+  }
+  
+  // Add nutritional information if possible
+  try{
+    const nutrition:ProductNutrition = {
+      servings: getNumFromString(rawProductJson.nutrition.servingsPerPackage)[0],
+      servingSize: getNumFromString(rawProductJson.nutrition.servingSize)[0],
+      kilojoules: 0,
+      protein: 0,
+      fat: 0,
+      fatSaturated: 0,
+      carb: 0,
+      sugar: 0,
+      sodium: 0
+    }
+
+    const nutritionSize = getNumFromString(rawProductJson.nutrition.breakdown[0].title)[0]
+    const scaleNutrient = nutrition.servingSize / nutritionSize
+
+    // Extract 7 mandatory labeled
+    rawProductJson.nutrition.breakdown[0].nutrients.forEach((nutrient:any) => {
+      switch (nutrient.nutrient) {
+        case 'Energy':
+          nutrition.kilojoules = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Protein':
+          nutrition.protein = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Total Fat':
+          nutrition.fat = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Saturated Fat':
+          nutrition.fatSaturated = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Carbohydrate':
+          nutrition.carb = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Sugars':
+          nutrition.sugar = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        case 'Sodium':
+          nutrition.sodium = roundDecimal(getNumFromString(nutrient.value)[0] * scaleNutrient, 2)
+          break
+        default:
+      }
+    })
+
+    productInfo.nutrition = nutrition
+  } catch (err:any) {}
+  return productInfo
+}
+
+
+
+export const getWoolworthsBatchProductInfo:GetBatchProductInfo = async(urls) => {
+  const productInfos:ProductInfo[] = []
+  for (const url of urls) {
+    const html = await scrapeDynamic(url)
+    const productInfo = getWoolworthsProductInfo(html)
+    if (productInfo != null) { productInfos.push(productInfo) }
+  }
+  return productInfos
+}
