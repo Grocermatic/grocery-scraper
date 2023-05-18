3f77d5be5155fb1545aa1ff5049841d7a2fd27bb
MengLinMaker <menglinmaker@gmail.com>
2023-05-18 12:54:27 +1000

    Add unit extraction helper function

--- webscrape/src/util/dataCleaning.ts
+++ webscrape/src/util/dataCleaning.ts
@@ -1,6 +1,5 @@
 export const getNumFromString = (str:string):number[] => {
-  const regex = /\d+(\.\d+)?/g;
-  const matches = str.match(regex);
+  const matches = str.match(/\d+(\.\d+)?/g) // Match integers and decimals
   if (matches) {
     let numArray:number[] = []
     matches.forEach((num:string) => {
@@ -11,6 +10,17 @@ export const getNumFromString = (str:string):number[] => {
   return [];
 }
 
+
+
 export const roundDecimal = (decimal:number, places:number):number => {
   return Math.round(decimal * 10**places) / 10**places
+}
+
+
+
+export const getUnitFromString = (str:string):string => {
+  let unitMeasure = str.slice(-2).toLowerCase() // Units are assumed to be 1-2 characters
+  unitMeasure = unitMeasure.replace(/[^a-z]/g, '') // Only extract letters
+  if (!['l','ml','kg','g'].includes(unitMeasure)) { return '' }
+  return unitMeasure
 }
