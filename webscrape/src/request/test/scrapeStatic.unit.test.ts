416382a7f008449c5783dc451e18139de9ff59fe
MengLinMaker <menglinmaker@gmail.com>
2023-05-18 13:23:16 +1000

    Clarify static scrape test

--- webscrape/src/request/test/scrapeStatic.unit.test.ts
+++ webscrape/src/request/test/scrapeStatic.unit.test.ts
@@ -7,19 +7,17 @@ import {scrapeStatic} from '../scrapeStatic'
 
 describe("static html scraper", () => {
   it("should render javascript html", async()=>{
-    const data = fs.readFileSync('./src/request/test.html')
-    const originalHTML = data.toString()
+    const testUrl = `${__dirname}/test.html`
+    const originalHTML = fs.readFileSync(testUrl).toString()
 
     http.createServer((req:any, res:any) => {
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.write(originalHTML)
         res.end()
     }).listen(3000)
-
-    scrapeStatic('http://localhost:3000/')
-    .then((html:string) => {
-      expect(html).toEqual(originalHTML)
-    })
+    
+    const html = await scrapeStatic('http://localhost:3000/')
+    expect(html).toEqual(originalHTML)
   })
 
   it("should handle unreachable urls", async()=>{
