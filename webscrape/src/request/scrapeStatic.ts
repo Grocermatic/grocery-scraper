96fc1da3d0dae9c43a2e52dcc4091b2daf97d45c
MengLinMaker <menglinmaker@gmail.com>
2023-05-20 19:56:31 +1000

    Add optional cookie to statsic scrape

--- webscrape/src/request/scrapeStatic.ts
+++ webscrape/src/request/scrapeStatic.ts
@@ -4,13 +4,18 @@ import { wait } from '../util/wait'
 
 
 
-export const scrapeStatic = async(url:string):Promise<any> => {
+export const scrapeStatic = async(url:string, cookie?:string):Promise<any> => {
+  const headers = generateHttpHeaders()['headers']
+  if (cookie != undefined) {
+    headers.Cookie = cookie
+  }
   await wait(generateRandInt(1000,3000))
+  
   try {
     const proxyClient = axios.create({
       baseURL: `https://${generatePublicIP()}`,
       timeout: 3000,
-      headers: generateHttpHeaders()['headers']
+      headers: headers
     })
     const response = await proxyClient.get(url)
     return response.data
