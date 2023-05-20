02fb40bbff23cfda910e43c8b9690f19a21747bc
MengLinMaker <menglinmaker@gmail.com>
2023-05-20 19:56:45 +1000

    Add Json scrape capability

---
+++ webscrape/src/request/scrapeJson.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { generateHttpHeaders, generatePublicIP, generateRandInt } from './proxy'
+import { wait } from '../util/wait'
+
+
+
+export const scrapeJson = async(url:string, cookie?:string):Promise<any> => {
+  const headers = generateHttpHeaders()['headers']
+  if (cookie != undefined) {
+    headers.Cookie = cookie
+  }
+  await wait(generateRandInt(1000,3000))
+  
+  try {
+    const proxyClient = axios.create({
+      baseURL: `https://${generatePublicIP()}`,
+      timeout: 3000,
+      headers: headers
+    })
+    const response = await proxyClient.get(url)
+    return JSON.parse(response.data)
+  } catch (err:any) {
+    return ''
+  } 
+}
