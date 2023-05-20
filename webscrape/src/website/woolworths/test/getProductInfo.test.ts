0451a7d1eb537e166663e737874661ad2b3ff3de
MengLinMaker <menglinmaker@gmail.com>
2023-05-21 01:07:27 +1000

    Alter Woolworths scrape test to suite json

--- webscrape/src/website/woolworths/test/getProductInfo.test.ts
+++ webscrape/src/website/woolworths/test/getProductInfo.test.ts
@@ -6,7 +6,7 @@ import { ProductInfo } from '../../interface'
 
 
 describe("Woolworths product scraper", () => {
-  it("should parse nutrition data", async()=>{
+  it("should not parse servingSize and fatSaturated: onion", async()=>{
     const testUrl = `${__dirname}/onion.test.json`
     const html:string = await fs.readFileSync(testUrl).toString()
     const productInfo = getWoolworthsProductInfo(html)
@@ -18,7 +18,7 @@ describe("Woolworths product scraper", () => {
       quantity: 1,
       unitPrice: 3.2,
       nutrition: {
-        servingSize: 0,
+        servingSize: null,
         kilojoules: 120,
         protein: 1.6,
         fat: 0.1,
@@ -31,92 +31,37 @@ describe("Woolworths product scraper", () => {
     expect(productInfo).toEqual(expectedProductInfo)
   })
 
-  it("should parse nutrition data", async()=>{
-    const testUrl = `${__dirname}/milk.test.html`
+  it("should parse all data: milk", async()=>{
+    const testUrl = `${__dirname}/milk.test.json`
     const html:string = await fs.readFileSync(testUrl).toString()
     const productInfo = getWoolworthsProductInfo(html)
     const expectedProductInfo:ProductInfo = {
-      name: 'Woolworths Whole Milk 3l',
+      name: 'Woolworths Lite Milk',
       url: 'https://www.woolworths.com.au/shop/productdetails/888141',
-      img: 'https://cdn0.woolworths.media/content/wowproductimages/large/807384.jpg',
+      img: 'https://cdn0.woolworths.media/content/wowproductimages/large/888141.jpg',
       price: 4.5,
       quantity: 3,
       unitPrice: 1.5,
       nutrition: {
         servingSize: 0.25,
-        kilojoules: 659,
-        protein: 8.3,
-        fat: 8.5,
-        fatSaturated: 5.5,
-        carb: 12,
-        sugar: 12,
-        sodium: 110
+        kilojoules: 189,
+        protein: 3.4,
+        fat: 1.3,
+        fatSaturated: 0.9,
+        carb: 4.9,
+        sugar: 4.9,
+        sodium: 43
       }
     }
     expect(productInfo).toEqual(expectedProductInfo)
   })
 
-  it("should parse nutrition data", async()=>{
-    const testUrl = `${__dirname}/sweetPotato.test.html`
+  it("should extrapolate nutrition info: potato", async()=>{
+    const testUrl = `${__dirname}/potato.test.json`
     const html:string = await fs.readFileSync(testUrl).toString()
     const productInfo = getWoolworthsProductInfo(html)
     const expectedProductInfo:ProductInfo = {
-      name: 'Woolworths Gold Sweet Potatoes Bag 1kg',
-      url: 'https://www.woolworths.com.au/shop/productdetails/156015',
-      img: 'https://cdn0.woolworths.media/content/wowproductimages/large/156015.jpg',
-      price: 4.5,
-      quantity: 1,
-      unitPrice: 4.5,
-      nutrition: {
-        servingSize: 0.077,
-        kilojoules: 185,
-        protein: 1.4,
-        fat: 0.1,
-        fatSaturated: 0.1,
-        carb: 10.6,
-        sugar: 4.2,
-        sodium: 8
-      }
-    }
-    expect(productInfo).toEqual(expectedProductInfo)
-  })
-
-  it("should not include invalid nutrition data", async()=>{
-    const testUrl = `${__dirname}/raspberry.test.html`
-    const html:string = await fs.readFileSync(testUrl).toString()
-    const productInfo = getWoolworthsProductInfo(html)
-    const expectedProductInfo:ProductInfo = {
-      name: 'Fresh Raspberry 125g',
-      url: 'https://www.woolworths.com.au/shop/productdetails/165262',
-      img: 'https://cdn0.woolworths.media/content/wowproductimages/large/165262.jpg',
-      price: 5,
-      quantity: 0.125,
-      unitPrice: 40,
-    }
-    expect(productInfo).toEqual(expectedProductInfo)
-  })
-
-  it("should not include invalid nutrition data", async()=>{
-    const testUrl = `${__dirname}/blackberry.test.html`
-    const html:string = await fs.readFileSync(testUrl).toString()
-    const productInfo = getWoolworthsProductInfo(html)
-    const expectedProductInfo:ProductInfo = {
-      name: 'Blackberry Fresh 125g',
-      url: 'https://www.woolworths.com.au/shop/productdetails/296813',
-      img: 'https://cdn0.woolworths.media/content/wowproductimages/large/296813.jpg',
-      price: 5,
-      quantity: 0.125,
-      unitPrice: 40,
-    }
-    expect(productInfo).toEqual(expectedProductInfo)
-  })
-
-  it("should not include invalid nutrition data", async()=>{
-    const testUrl = `${__dirname}/potato.test.html`
-    const html:string = await fs.readFileSync(testUrl).toString()
-    const productInfo = getWoolworthsProductInfo(html)
-    const expectedProductInfo:ProductInfo = {
-      name: 'Woolworths Brushed Potatoes Bag 4kg',
+      name: 'Woolworths Brushed Potatoes Bag',
       url: 'https://www.woolworths.com.au/shop/productdetails/124831',
       img: 'https://cdn0.woolworths.media/content/wowproductimages/large/124831.jpg',
       price: 9,
@@ -124,13 +69,13 @@ describe("Woolworths product scraper", () => {
       unitPrice: 2.25,
       nutrition: {
         servingSize: 0.075,
-        kilojoules: 185,
-        protein: 1.7,
+        kilojoules: 246.67,
+        protein: 2.3,
         fat: 0.1,
         fatSaturated: 0.1,
-        carb: 9.7,
-        sugar: 0.5,
-        sodium: 0
+        carb: 12.9,
+        sugar: 0.67,
+        sodium: 4
       }
     }
     expect(productInfo).toEqual(expectedProductInfo)
