3454d06f22908e943852dcc1086bcba000b21bf2
MengLinMaker <menglinmaker@gmail.com>
2023-05-22 13:48:40 +1000

    Filter out unit price below $20

--- webscrape/src/website/coles/getProductLinks.ts
+++ webscrape/src/website/coles/getProductLinks.ts
@@ -4,7 +4,7 @@ import * as cheerio from "cheerio";
 
 import { GetProductLinks } from "../interface";
 import { scrapeStatic } from "../../request/scrapeStatic";
-import { generateUniqueArray } from "../../util/dataCleaning";
+import { generateUniqueArray, getUnitPriceFromString } from "../../util/dataCleaning";
 
 
 
@@ -14,23 +14,34 @@ const getColesSectionProductLinks = async(pageLink:string):Promise<string[]> => 
 
   for (let pageNumber = 1;;pageNumber++) {
 
-    const pageHtml = await scrapeStatic(pageLink + `?page=${pageNumber}`)
+    const pageHtml = await scrapeStatic(pageLink + `?sortBy=unitPriceAscending&page=${pageNumber}`)
     const $ = cheerio.load(pageHtml)
 
     // Focus on extracting from product grid
     const sectionElement = $('section.coles-targeting-ProductTileProductTileWrapper')
     if (sectionElement.length == 0) { break }
 
-    sectionElement.each((index, element) => {
+    const unitPriceThreshold = 20
+    let totalUnitPrice = 0
+    let numberOfProducts = 0
+    for (const element of sectionElement) {
       const productLink = $(element).find('a.product__link').attr('href')
       const unitPriceString = $(element).find('div.product__pricing_area').find('span.price__calculation_method').text()
       const productAvailable = $(element).find('span.coles-targeting-ButtonButtonChildContainer').text() == 'Add'
-      if (productLink == undefined) { return }
-      if (!productAvailable) {return }
-      if (unitPriceString.includes('ea')) { return } // Include only measurable products
-      if (productLinks.includes(productLink)) { return }
-      productLinks.push(baseUrl + productLink)
-    })
+      if (productLink == undefined) {}
+      else if (!productAvailable) {}
+      else if (unitPriceString.includes('ea')) {} // Include only measurable products
+      else if (productLinks.includes(productLink)) {}
+      else {
+        productLinks.push(baseUrl + productLink)
+        numberOfProducts++
+        totalUnitPrice += getUnitPriceFromString(unitPriceString)
+      }
+    }
+
+    // Limit products above price threshold
+    const averageUnitPrice = totalUnitPrice / numberOfProducts
+    if ((numberOfProducts != 0) && (averageUnitPrice > unitPriceThreshold)) { break }
   }
   return productLinks
 }
@@ -43,9 +54,13 @@ export const getColesProductLinks:GetProductLinks = async() => {
     'https://www.coles.com.au/browse/fruit-vegetables',
     'https://www.coles.com.au/browse/dairy-eggs-fridge',
     'https://www.coles.com.au/browse/bakery',
-    'https://www.coles.com.au/browse/pantry',
+    'https://www.coles.com.au/browse/pantry/pasta-rice-legumes-grains',
+    'https://www.coles.com.au/browse/pantry/pickled-vegetables-condiments',
+    'https://www.coles.com.au/browse/pantry/stocks-gravy',
+    'https://www.coles.com.au/browse/pantry/health-foods',
+    'https://www.coles.com.au/browse/pantry/canned-food-soups-noodles',
+    'https://www.coles.com.au/browse/pantry/breakfast',
     'https://www.coles.com.au/browse/deli',
-    'https://www.coles.com.au/browse/drinks',
     'https://www.coles.com.au/browse/frozen',
     'https://www.coles.com.au/browse/meat-seafood'
   ]
