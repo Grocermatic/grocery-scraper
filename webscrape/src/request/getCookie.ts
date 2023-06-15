0c0d5da62d946a0045b8c5d54a4a74d7ea1dd5f6
Meng Lin <menglinmaker@gmail.com>
2023-06-15 21:15:02 +1000

    Add cloud compatible chromium

--- webscrape/src/request/getCookie.ts
+++ webscrape/src/request/getCookie.ts
@@ -1,54 +1,24 @@
 /* istanbul ignore file */
 
-import puppeteer from 'puppeteer'
-import { generateHttpHeaders } from './proxy'
+import puppeteer, { PuppeteerLaunchOptions } from 'puppeteer'
+const chromium = require("@sparticuz/chromium");
+
+import { generateHttpHeaders, generatePublicIP } from './proxy'
 
 
 
 export const getCookie = async(url:string):Promise<string> => {  
   // Disable sandbox to decrease scrape time
-  const browser = await puppeteer.launch({
-    headless: 'new',
-    args: [
-      '--autoplay-policy=user-gesture-required',
-      '--disable-background-networking',
-      '--disable-background-timer-throttling',
-      '--disable-backgrounding-occluded-windows',
-      '--disable-breakpad',
-      '--disable-client-side-phishing-detection',
-      '--disable-component-update',
-      '--disable-default-apps',
-      '--disable-dev-shm-usage',
-      '--disable-domain-reliability',
-      '--disable-extensions',
-      '--disable-features=AudioServiceOutOfProcess',
-      '--disable-hang-monitor',
-      '--disable-ipc-flooding-protection',
-      '--disable-notifications',
-      '--disable-offer-store-unmasked-wallet-cards',
-      '--disable-popup-blocking',
-      '--disable-print-preview',
-      '--disable-prompt-on-repost',
-      '--disable-renderer-backgrounding',
-      '--disable-setuid-sandbox',
-      '--disable-speech-api',
-      '--disable-sync',
-      '--hide-scrollbars',
-      '--ignore-gpu-blacklist',
-      '--metrics-recording-only',
-      '--mute-audio',
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--no-pings',
-      '--no-sandbox',
-      '--no-zygote',
-      '--password-store=basic',
-      '--use-gl=swiftshader',
-      '--use-mock-keychain',
-    ]
-  })
-
+  const launchOption:PuppeteerLaunchOptions = {
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless,
+    args: [...chromium.args]
+  }
+  launchOption.executablePath = await chromium.executablePath("/opt/bin")
+  const browser = await puppeteer.launch(launchOption)
+  
   const page = await browser.newPage()
+  await page.setDefaultNavigationTimeout(0);
   await page.setRequestInterception(true)
   await page.setExtraHTTPHeaders(generateHttpHeaders()['headers'])
 
@@ -63,7 +33,6 @@ export const getCookie = async(url:string):Promise<string> => {
     }
   })
 
-
   let cookies = ''
   try {
     await page.goto(url, {waitUntil: 'load'})
@@ -72,7 +41,7 @@ export const getCookie = async(url:string):Promise<string> => {
       cookies += `${cookieObject.name}=${cookieObject.value};`
     }
   } finally {
-    await browser.close()
+    browser.close()
   }
   return cookies
 }
