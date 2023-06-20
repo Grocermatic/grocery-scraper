7939385c7062267dfe5e87982b00338241bf975c
Meng Lin <menglinmaker@gmail.com>
2023-06-20 12:14:10 +1000

    Array redistributor

--- webscrape/src/util/dataCleaning.ts
+++ webscrape/src/util/dataCleaning.ts
@@ -78,3 +78,27 @@ export const getUnitPriceFromString = (unitPriceImplicitString:string):number =>
   const unitPrice = unitPriceImplicit / unitQuantityImplicit
   return unitPrice
 }
+
+
+
+export const limitArrayLengths = (oldArrays:any[][], desiredArrayLength:number) => {
+  const newArrays:any[] = []
+
+  const lengthOfEachArray = oldArrays.map((val)=>{return val.length})
+  const maxArrayLength = Math.max(...lengthOfEachArray)
+
+  for (let arrayLengthCounter = 0; arrayLengthCounter < maxArrayLength; arrayLengthCounter += desiredArrayLength) {
+    const subsetArrays = []
+    for (let arrayID = 0; arrayID < oldArrays.length; arrayID++) {
+      const arraySample = oldArrays[arrayID].slice(0,desiredArrayLength)
+      if (arraySample.length > 0) {
+        subsetArrays.push(arraySample)
+      }
+      oldArrays[arrayID] = oldArrays[arrayID].slice(desiredArrayLength)
+    }
+    if (subsetArrays.length > 0) {
+      newArrays.push(subsetArrays)
+    }
+  }
+  return newArrays
+}
