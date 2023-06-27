fa4ab2ad43bbe66b73e1981bdc46b30b157c7730
Meng Lin <menglinmaker@gmail.com>
2023-06-28 09:54:52 +1000

    Refactor liveScrape to getProductInfo script

---
+++ webscrape/script/getProductInfo.ts
@@ -0,0 +1,49 @@
+import * as fs from 'fs'
+
+import { getAllProductInfos } from '../src/website/getAllProductInfo'
+import { roundDecimal } from '../src/dataCleaning/roundDecimal'
+
+
+
+const logFilePath = './script/liveScrape.log'
+
+const writePerformanceToLog = (performaneMeasure:PerformanceMeasure) => {
+  console.log(performaneMeasure)
+  const duration = roundDecimal(performaneMeasure.duration / 1000, 3)
+  const logMessage = `${performaneMeasure.name}, ${duration}\n`
+  fs.appendFileSync(logFilePath, logMessage)
+}
+
+
+
+const writeMessageToLog = (message:string) => {
+  console.log(message)
+  fs.appendFileSync(logFilePath, `${message}\n`)
+}
+
+
+
+export const getProductInfo = () => {
+const filePath = `./data`
+  const stores = ['aldi', 'coles', 'woolworths']
+
+  stores.map(async(store) => {
+    const urlsList = fs.readFileSync(`${filePath}/${store}Links.csv`).toString().split('\n')
+
+    writeMessageToLog(`Scrape ${store} productInfo:`)
+    performance.mark('Start scraping')
+    const productInfos = await getAllProductInfos([urlsList], 10)
+    performance.mark('Finish scraping')
+    writePerformanceToLog(performance.measure('Scrape time (s)', 'Start scraping', 'Finish scraping'))
+    writeMessageToLog('')
+  
+    const content = JSON.stringify(productInfos, null, 2)
+    fs.writeFileSync(filePath + `${store}ProductInfo.json`, content)
+  })
+}
+
+
+
+(async() => {
+  getProductInfo()
+})()
