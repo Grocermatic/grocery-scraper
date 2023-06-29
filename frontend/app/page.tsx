a351ee5365996b6fa0aaa7790d7f56a8e826a939
Meng Lin <menglinmaker@gmail.com>
2023-06-29 13:21:23 +1000

    Add test Home page with grocery cards

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import GroceryCard from "@/components/cards/GroceryCard"
+
+const Home = () => {
+  return (
+    <div>
+      <GroceryCard/>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/components/cards/GroceryCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Image from 'next/image'
+
+const GroceryCard = () => {
+  return (
+    <div>
+      GroceryCard
+      <Image 
+        src='https://productimages.coles.com.au/productimages/5/5708442.jpg'
+        width={200}
+        height={200}
+        alt=''
+      />
+      <Image 
+        src='https://productimages.coles.com.au/productimages/5/5708442.jpg'
+        width={200}
+        height={200}
+        alt=''
+      />
+      <Image 
+        src='https://productimages.coles.com.au/productimages/5/5708442.jpg'
+        width={200}
+        height={200}
+        alt=''
+      />
+    </div>
+  )
+}
+
+export default GroceryCard
