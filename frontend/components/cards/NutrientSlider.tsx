85bfe8b6ebda44860c52cb6812d60282e3819bf2
Meng Lin <menglinmaker@gmail.com>
2023-07-02 12:02:35 +1000

    Remove text and emphasize bar graph

--- frontend/components/cards/NutrientSlider.tsx
+++ frontend/components/cards/NutrientSlider.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import styles from './NutrientSlider.module.css'
+import React from "react"
+import styles from "./NutrientSlider.module.css"
 
-const NutrientSlider = ({nutrient, value, rdi}:any) => {
+const NutrientSlider = ({ nutrient, value, rdi }: any) => {
+  const hiddenExtension = 100
   return (
     <>
-      { value &&
-      <div className={styles.nutrientSlider}>
-        <p className={styles.labelText}>{nutrient}</p>
-        <div className={styles.sliderBase}>
-          <div className={styles.sliderFill} style={{width: `${100 * value / rdi}%`}}/>
+      {value && (
+        <div className={styles.nutrientSlider}>
+          <p className={styles.labelText}>{nutrient}</p>
+          <div className={styles.sliderBase}>
+            {value > rdi && (
+              <div
+                className={styles.sliderFillOver}
+                style={{ width: `${(100 * value) / rdi}%` }}
+              />
+            )}
+            {value <= rdi && (
+              <div
+                className={styles.sliderFill}
+                style={{
+                  marginLeft: `-${hiddenExtension}%`,
+                  width: `${(100 * value) / rdi + hiddenExtension}%`,
+                }}
+              />
+            )}
+          </div>
         </div>
-        <p className={styles.valueText}>{`${value} / ${rdi}`}</p>
-      </div>
-      }
+      )}
     </>
   )
 }
