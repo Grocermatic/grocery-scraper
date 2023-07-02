35725399798f232ec943402dd6e290bf233b5abe
Meng Lin <menglinmaker@gmail.com>
2023-07-02 12:28:39 +1000

    Add overshoot representation

--- frontend/components/cards/NutrientSlider.tsx
+++ frontend/components/cards/NutrientSlider.tsx
@@ -3,24 +3,31 @@ import styles from "./NutrientSlider.module.css"
 
 const NutrientSlider = ({ nutrient, value, rdi }: any) => {
   const hiddenExtension = 100
+  const percentage = value / rdi
+  const styleBase = percentage > 1 ? styles.sliderFillOver : styles.sliderBase
   return (
     <>
       {value && (
         <div className={styles.nutrientSlider}>
           <p className={styles.labelText}>{nutrient}</p>
-          <div className={styles.sliderBase}>
+          <div className={styleBase}>
             {value > rdi && (
-              <div
-                className={styles.sliderFillOver}
-                style={{ width: `${(100 * value) / rdi}%` }}
-              />
+              <>
+                <div
+                  className={styles.sliderFill}
+                  style={{
+                    marginLeft: `-${hiddenExtension}%`,
+                    width: `${100 / percentage + hiddenExtension}%`,
+                  }}
+                />
+              </>
             )}
             {value <= rdi && (
               <div
                 className={styles.sliderFill}
                 style={{
                   marginLeft: `-${hiddenExtension}%`,
-                  width: `${(100 * value) / rdi + hiddenExtension}%`,
+                  width: `${100 * percentage + hiddenExtension}%`,
                 }}
               />
             )}
