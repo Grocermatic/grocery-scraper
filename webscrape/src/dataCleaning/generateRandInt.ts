a05441b6146dc3d5b504bf26957fe5f4f4b99744
Meng Lin <menglinmaker@gmail.com>
2023-10-09 21:37:11 +1100

    Delete extraneous shuffle function

--- webscrape/src/dataCleaning/generateRandInt.ts
+++
@@ -1,6 +0,0 @@
-/* istanbul ignore file */
-
-export const generateRandInt = (min:number, max:number):number => {
-  const randomInteger = min + Math.floor((max - min + 1) * Math.random())
-  return randomInteger <= max ? randomInteger : randomInteger - 1
-}
