b8347ff9f06ee7d908aad4363e373015f61f38d6
Meng Lin <menglinmaker@gmail.com>
2023-10-11 01:06:21 +1100

    Implicitly add rounding

--- webscrape/src/dataCleaning/getUnitPriceFromString.ts
+++ webscrape/src/dataCleaning/getUnitPriceFromString.ts
@@ -1,11 +1,12 @@
 import { getMetricQuantity } from "./getMetricQuantity"
 import { getNumFromString } from "./getNumFromString"
+import { roundDecimal } from "./roundDecimal"
 
 
 
 export const getUnitPriceFromString = (unitPriceImplicitString:string):number => {
   const unitQuantityImplicit = getMetricQuantity(unitPriceImplicitString)
   const unitPriceImplicit = getNumFromString(unitPriceImplicitString)[0]
-  const unitPrice = unitPriceImplicit / unitQuantityImplicit
+  const unitPrice = roundDecimal(unitPriceImplicit / unitQuantityImplicit, 2)
   return unitPrice
 }
