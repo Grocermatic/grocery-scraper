7e9f62f3e8b9150fb04f0e671028e01ad690c20a
Meng Lin <menglinmaker@gmail.com>
2023-10-11 01:11:48 +1100

    Scrape from section pages

--- webscrape/src/website/coles/getProductInfo.ts
+++ webscrape/src/website/coles/getProductInfo.ts
@@ -3,45 +3,43 @@ import * as cheerio from 'cheerio'
 import { ProductInfo, GetProductInfo, GetBatchProductInfo } from "../interface"
 import { getUnitPriceFromString } from "../../dataCleaning/getUnitPriceFromString";
 import { scrapeStatic } from '../../request/scrapeStatic';
-import { roundDecimal } from '../../dataCleaning/roundDecimal';
 import { getMetricQuantity } from '../../dataCleaning/getMetricQuantity';
 
 
 
-export const getColesProductInfo:GetProductInfo = (html) => {
-  // Coles provides information rich JSON in script
-  const $ = cheerio.load(html)
-  let jsonString = $('#__NEXT_DATA__').text()
-  if (jsonString === '') return null
+export const getColesProductInfo = (product:any) => {
+  const urlSlug = `${product.brand}-${product.name}-${product.size}-${product.id}`.toLowerCase().split(' ').join('-')
+  
+  const productUnit = product.pricing.unit
+  let quantity = getMetricQuantity(`${productUnit.quantity * productUnit.ofMeasureQuantity}${productUnit.ofMeasureUnits}`)
+  try { quantity = getMetricQuantity(product.size) } catch {}
 
-  const rawJson = JSON.parse(jsonString)
-  const url = `https://www.coles.com.au/product/${rawJson['query']['slug']}`
-
-  try {
-    const rawProductJson = rawJson['props']['pageProps']['product']
-    if (rawProductJson['pricing'] == null) return null
-
-    const unitPriceImplicitString = $('.price__calculation_method').first().text().split(' | ')[0]
-    const unitPrice = getUnitPriceFromString(unitPriceImplicitString)
-    const productTitleString = $('h1.product__title').first().text()
-    const quantity = getMetricQuantity(productTitleString)
-    
-    const productInfo:ProductInfo = {
-      name: rawProductJson['name'],
-      url: url,
-      img: `https://productimages.coles.com.au/productimages${rawProductJson['imageUris'][0]['uri']}`,
-      price: rawProductJson['pricing']['now'],
-      quantity: roundDecimal(quantity, 3),
-      unitPrice: roundDecimal(unitPrice, 2)
-    }
-    if (Object.values(productInfo).includes(null)){
-      throw new Error(`ProductInfo contains null: ${JSON.stringify(productInfo)}`)
-    }
-    return productInfo
-  } catch {
-    console.log(`Cannot scrape: ${url}`)
-    return null
+  const productInfo:ProductInfo = {
+    name: product.name,
+    url: `https://www.coles.com.au/product/${urlSlug}`,
+    img: `https://productimages.coles.com.au/productimages${product.imageUris[0].uri}`,
+    price: product.pricing.now,
+    quantity: getMetricQuantity(product.size),
+    unitPrice: getUnitPriceFromString(product.pricing.comparable)
   }
+  return productInfo
+}
+
+
+
+export const colesPageProducts = (jsonData:any) => {
+  const productInfos:ProductInfo[] = []
+
+  // Filter non-available products and quantities with each
+  const products = JSON.parse(jsonData).props.pageProps.searchResults.results.filter((product:any) => {
+    return product.availability == true && product.pricing.unit.ofMeasureUnits != 'ea'
+  })
+
+  for (const product of products) {
+    const productInfo = getColesProductInfo(product)
+    productInfos.push(productInfo)
+  }
+  return productInfos
 }
 
 
