8e7ed7043195714da74012a75b11c4d490bc4a25
Meng Lin <menglinmaker@gmail.com>
2023-10-11 12:32:44 +1100

    Add wait between JSON scraping

--- webscrape/src/request/scrapeJson.ts
+++ webscrape/src/request/scrapeJson.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 
 import axios from 'axios'
+import { wait } from './wait'
 
 
 
@@ -10,6 +11,7 @@ export const getRequestJson = async(url:string, cookie?:string):Promise<string> 
   try {
     const proxyClient = axios.create({headers: headers})
     const response = await proxyClient.get(url)
+    await wait(1000)
     return JSON.stringify(response.data)
   } catch {
     return ''
@@ -24,6 +26,7 @@ export const postRequestJson = async(url:string, postRequestPayload:any, cookie?
   try {
     const proxyClient = axios.create({headers: headers})
     const response = await proxyClient.post(url, postRequestPayload)
+    await wait(1000)
     return JSON.stringify(response.data)
   } catch {
     return ''
