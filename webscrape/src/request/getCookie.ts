cf8d5016dd8a3eb6a6b8fe26073c86d858d17814
Meng Lin <menglinmaker@gmail.com>
2023-10-13 20:24:49 +1100

    Streamline cookie scraping

--- webscrape/src/request/getCookie.ts
+++ webscrape/src/request/getCookie.ts
@@ -1,39 +1,39 @@
 /* istanbul ignore file */
 
 import puppeteer, { PuppeteerLaunchOptions } from 'puppeteer'
-const chromium = require("@sparticuz/chromium");
+const chromium = require("@sparticuz/chromium")
 
 
 
-export const getCookie = async(url:string):Promise<string> => {  
-  const launchOption:PuppeteerLaunchOptions = {
-    defaultViewport: chromium.defaultViewport,
-    headless: chromium.headless,
-    args: [...chromium.args]
-  }
-  try {
-    launchOption.executablePath = await chromium.executablePath("/opt/bin")
-  } catch {}
+const launchOption: PuppeteerLaunchOptions = {
+  defaultViewport: chromium.defaultViewport,
+  headless: 'new'
+}
+
+chromium.executablePath("/opt/bin").then((path: string) => {
+  launchOption.executablePath = path
+}).catch(() => {})
+
+const excludeContentType = ['EventSource', 'Fetch', 'Font', 'Image', 'Manifest', 'Media', 'Script', 'Stylesheet', 'TextTrack', 'WebSocket', 'XHR']
+
+
+
+export const getCookie = async (url: string): Promise<string> => {
+  let cookies = ''
+
   const browser = await puppeteer.launch(launchOption)
-  
   const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(0);
+
   await page.setRequestInterception(true)
 
   // Block extaneous information to decrease scrape time
-  const excludeContentType = ['image', 'media', 'stylesheet']
-  page.on('request', (req:any) => {
-    if(excludeContentType.includes(req.resourceType())){
-        req.abort();
-    }
-    else {
-        req.continue();
-    }
+  page.on('request', (req: any) => {
+    if (excludeContentType.includes(req.resourceType())) { req.abort() }
+    else { req.continue() }
   })
 
-  let cookies = ''
   try {
-    await page.goto(url, {waitUntil: 'domcontentloaded'})
+    await page.goto(url, { waitUntil: 'domcontentloaded' })
     const cookiesArray = await page.cookies(url)
     for (const cookieObject of cookiesArray) {
       cookies += `${cookieObject.name}=${cookieObject.value};`
