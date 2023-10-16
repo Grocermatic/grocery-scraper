66ef4867b30de4da27a271540db211c88317a25b
Meng Lin <menglinmaker@gmail.com>
2023-10-17 01:36:22 +1100

    Create Coles main scraper

---
+++ webscrape/src/website/coles/scrapeColes.ts
@@ -0,0 +1,50 @@
+import { ProductInfoReport } from "../ProductInfoReport"
+import { getProductInfoSection } from "./getProductInfoSection"
+
+
+
+export const scrapeColes = async (cookie: string) => {
+  const report = new ProductInfoReport()
+
+  // Page links with get request health star filters
+  const sectionLinks = [
+    // fruit-vegetables
+    'https://www.coles.com.au/browse/fruit-vegetables',
+    // dairy-eggs-fridge
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/cheese',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/dairy',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/eggs',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/fresh-pasta-sauces',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/long-life-milk',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/milk',
+    'https://www.coles.com.au/browse/dairy-eggs-fridge/vegetarian-vegan',
+    // bakery
+    'https://www.coles.com.au/browse/bakery',
+    // deli
+    'https://www.coles.com.au/browse/deli',
+    // pantry
+    'https://www.coles.com.au/browse/pantry/baking',
+    'https://www.coles.com.au/browse/pantry/breakfast',
+    'https://www.coles.com.au/browse/pantry/canned-food-soups-noodles',
+    'https://www.coles.com.au/browse/pantry/coffee',
+    'https://www.coles.com.au/browse/pantry/herbs-spices',
+    'https://www.coles.com.au/browse/pantry/international-foods',
+    'https://www.coles.com.au/browse/pantry/jams-honey-spreads',
+    'https://www.coles.com.au/browse/pantry/oils-vinegars',
+    'https://www.coles.com.au/browse/pantry/pasta-rice-legumes-grains',
+    'https://www.coles.com.au/browse/pantry/pickled-vegetables-condiments',
+    'https://www.coles.com.au/browse/pantry/sauces',
+    'https://www.coles.com.au/browse/pantry/stocks-gravy',
+    // frozen
+    'https://www.coles.com.au/browse/frozen',
+    // meat-seafood
+    'https://www.coles.com.au/browse/meat-seafood'
+  ]
+
+  const promiseArray = sectionLinks.map(sectionLink => {
+    return report.recordProductInfoSection(getProductInfoSection, sectionLink, cookie)
+  })
+  await Promise.all(promiseArray)
+
+  return report.removeDuplicate().sortProductInfoUnitPrice().recordScrapeSecond()
+}
