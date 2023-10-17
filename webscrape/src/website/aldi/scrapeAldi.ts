0a0ef2d4e4affbb5212cdf85437704b114c7fc67
Meng Lin <menglinmaker@gmail.com>
2023-10-17 22:06:21 +1100

    Scrape sequentially

--- webscrape/src/website/aldi/scrapeAldi.ts
+++ webscrape/src/website/aldi/scrapeAldi.ts
@@ -7,17 +7,16 @@ export const scrapeAldi = async (cookie?: string) => {
   const report = new ProductInfoReport()
 
   const productLinks = [
-    'https://www.aldi.com.au/en/groceries/super-savers/',
-    'https://www.aldi.com.au/en/groceries/limited-time-only/',
-    'https://www.aldi.com.au/en/groceries/seasonal-range/',
-    'https://www.aldi.com.au/en/groceries/freezer/',
-    'https://www.aldi.com.au/en/groceries/pantry/olive-oil/',
-    'https://www.aldi.com.au/en/groceries/fresh-produce/dairy-eggs/'
+    'https://www.aldi.com.au/en/groceries/super-savers',
+    'https://www.aldi.com.au/en/groceries/limited-time-only',
+    'https://www.aldi.com.au/en/groceries/seasonal-range',
+    'https://www.aldi.com.au/en/groceries/freezer',
+    'https://www.aldi.com.au/en/groceries/pantry/olive-oil',
+    'https://www.aldi.com.au/en/groceries/fresh-produce/dairy-eggs'
   ]
 
-  const promiseArray = productLinks.map(url => {
-    return report.recordProductInfoSection(getProductInfoSection, url, cookie)
-  })
-  await Promise.all(promiseArray)
+  for (const url of productLinks) {
+    await report.recordProductInfoSection(getProductInfoSection, url, cookie)
+  }
   return report.removeDuplicate().sortProductInfoUnitPrice().recordScrapeSecond()
 }
