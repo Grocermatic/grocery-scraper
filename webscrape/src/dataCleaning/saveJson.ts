dc1b7a54fd92147a98fa2e2087175a7592c428f0
Meng Lin <menglinmaker@gmail.com>
2023-10-17 23:20:07 +1100

    Save object as formated JSON

---
+++ webscrape/src/dataCleaning/saveJson.ts
@@ -0,0 +1,8 @@
+import * as fs from 'fs'
+
+
+
+export const saveJson = (path: string, object: object) => {
+  const reportJson = JSON.stringify(object, null, 2)
+  fs.writeFileSync(path, reportJson)
+}
