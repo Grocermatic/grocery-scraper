7c0a011f7171fd95b78fc6588fabb038064d88d4
Meng Lin <menglinmaker@gmail.com>
2023-10-18 22:54:17 +1100

    Merge scraped data with debug logs

--- webscrape/script/cleanProductInfo.ts
+++ webscrape/script/cleanProductInfo.ts
@@ -1,25 +1,42 @@
 import * as fs from 'fs'
 import { ProductInfo } from '../src/website/interface'
+import { saveJson } from '../src/dataCleaning/saveJson'
+import { ProductInfoReport } from '../src/website/ProductInfoReport'
 
 
 
-export const cleanProductInfo = async() => {
-  const filePath = 'webscrape/data'
-  let allProductInfos:ProductInfo[] = []
-  
-  const stores = ['aldi', 'coles', 'woolworths']
-  for (let i = 0; i < stores.length; i++) {
-    const store = stores[i]
-    const productInfos = JSON.parse(fs.readFileSync(`${filePath}/ProductInfo/${store}.json`).toString())
-    allProductInfos = allProductInfos.concat(productInfos)
-  }
-  allProductInfos.sort((a,b) => {return a.unitPrice - b.unitPrice})
-  
-  fs.writeFileSync(`${filePath}/cleanProductInfo.json`, JSON.stringify(allProductInfos, null, 2))
+const stringKiloByte = (val: string) => {
+  return Math.round(val.length * 0.001)
 }
 
+const basePath = 'webscrape/data'
+const sourcePath = `${basePath}/productInfo`
+const maxUnitPrice = 30
+const minUnitPrice = 0.5
 
+const report = new ProductInfoReport()
 
-(async() => {
-  await cleanProductInfo()
-})()
+const files = fs.readdirSync(sourcePath)
+for (const file of files) {
+  const reportJson = fs.readFileSync(`${sourcePath}/${file}`).toString()
+  const productInfoReport = new ProductInfoReport(JSON.parse(reportJson))
+  report.merge(productInfoReport)
+}
+const productInfoReport = report.removeDuplicate().sortProductInfoUnitPrice().get()
+
+console.table({
+  "Success": productInfoReport.productInfo.length,
+  "Invalid data": productInfoReport.failedProductInfo.length,
+  "Failed to scrape": productInfoReport.failedProduct.length,
+  "Failed sections": productInfoReport.failedSection.length
+})
+
+const allProductInfos = productInfoReport.productInfo.filter((productInfo: ProductInfo) => {
+  if (productInfo.unitPrice > maxUnitPrice) return false
+  if (productInfo.unitPrice < minUnitPrice) return false
+  return true
+})
+
+console.debug(`Output data size: ${stringKiloByte(JSON.stringify(allProductInfos))} kb`)
+
+saveJson(`${basePath}/cleanProductInfo.json`, allProductInfos)
