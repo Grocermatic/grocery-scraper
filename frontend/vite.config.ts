7dca6fa3eb95c195b3bbf381092a84d68b602366
Meng Lin <menglinmaker@gmail.com>
2023-10-19 09:15:49 +1100

    Don't compress files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    viteSingleFile()
+  ],
 })
