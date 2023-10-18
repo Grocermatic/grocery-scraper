793013296a6224a140c3affc047a8836809fccc7
Meng Lin <menglinmaker@gmail.com>
2023-10-19 09:35:28 +1100

    Add new line to json for prettier

--- webscrape/src/dataCleaning/saveJson.ts
+++ webscrape/src/dataCleaning/saveJson.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs'
 
 export const saveJson = (path: string, object: object) => {
-  const reportJson = JSON.stringify(object, null, 2)
+  const reportJson = JSON.stringify(object, null, 2) + '\n'
   fs.writeFileSync(path, reportJson)
 }
