b1c0517738b41649c2275da67321a06dce55e4ca
Meng Lin <menglinmaker@gmail.com>
2023-10-19 22:11:40 +1100

    Add PWA basics

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solid(), viteSingleFile()],
+  plugins: [solid(), VitePWA({ registerType: 'autoUpdate' })],
 })
