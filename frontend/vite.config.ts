ede4f7b44401a60d19745727b93399fc5194546e
Meng Lin <menglinmaker@gmail.com>
2023-10-20 09:26:58 +1100

    Enable PWA installability

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,30 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import { VitePWA } from 'vite-plugin-pwa'
 
+let faviconUrl = '/favicon.svg'
+
 export default defineConfig({
-  plugins: [solid(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    solid(),
+    VitePWA({
+      includeAssets: [faviconUrl],
+      manifest: {
+        name: 'Grocermatic',
+        theme_color: '#22C55E',
+        icons: [
+          {
+            src: faviconUrl,
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable',
+          },
+          {
+            src: faviconUrl,
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
