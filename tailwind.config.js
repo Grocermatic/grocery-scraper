67f52ec9728544ebdbc9561b5d009fc6f53ca61a
Meng Lin <menglinmaker@gmail.com>
2023-10-22 22:07:05 +1100

    Remove dark mode maintainence overhead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-import colors from "tailwindcss/colors"
-import defaultTheme from "tailwindcss/defaultTheme"
+import colors from 'tailwindcss/colors'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["frontend/**/*.{js,ts,jsx,tsx,html}"],
-  darkMode: "class",
+  content: ['frontend/**/*.{js,ts,jsx,tsx,html}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         primary: colors.green[500],
-        light: colors.white,
+        light: colors.neutral[100],
         dark: colors.neutral[800],
       },
       fontFamily: {
         // to change, update font in _document.js
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
       },
     },
   },
