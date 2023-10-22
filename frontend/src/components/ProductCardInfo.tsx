4da26a07108931212e8457dc8925d8e0e36f4c8a
Meng Lin <menglinmaker@gmail.com>
2023-10-23 02:08:03 +1100

    Create basic product info component

---
+++ frontend/src/components/ProductCardInfo.tsx
@@ -0,0 +1,19 @@
+import { limitStringLength } from '../logic/limitStringLength'
+
+export const ProductCardInfo = (props: any) => {
+  const { name, url, price, quantity, unitPrice } = props
+  const productName = limitStringLength(name, 45)
+  return (
+    <>
+      <a href={url} target="blank">
+        <h2 class="underline">{productName}</h2>
+      </a>
+      <div class="flex-grow"></div>
+      <div class="flex justify-between gap-4">
+        <p class="font-bold">${price}</p>
+        <p class="font-bold">{quantity}kg</p>
+        <p class="font-bold">{unitPrice} $/kg</p>
+      </div>
+    </>
+  )
+}
