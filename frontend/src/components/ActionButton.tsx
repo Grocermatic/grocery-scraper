744477d12bbbede560f01ebcc5c61511a5757eee
Meng Lin <menglinmaker@gmail.com>
2023-10-22 22:08:15 +1100

    ActionButton

---
+++ frontend/src/components/ActionButton.tsx
@@ -0,0 +1,12 @@
+import { children, mergeProps, splitProps } from 'solid-js'
+
+export const ActionButton = (props: any) => {
+  props = mergeProps({ class: '', onClick: () => {} }, props)
+  const [local, others] = splitProps(props, ['class', 'onClick'])
+  const c = children(() => props.children)
+  return (
+    <button onClick={local.onClick} class={`card ${local.class}`}>
+      {c()}
+    </button>
+  )
+}
