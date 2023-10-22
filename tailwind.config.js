df1978c54014c8c412c467646a5f0a10fff2d4a5
Meng Lin <menglinmaker@gmail.com>
2023-10-22 17:19:03 +1100

    Add global inline css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+import colors from "tailwindcss/colors"
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['frontend/index.html', 'frontend/src/**/*.{js,ts,jsx,tsx}'],
+  content: ["frontend/**/*.{js,ts,jsx,tsx,html}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.green[500],
+        light: colors.white,
+        dark: colors.neutral[800],
+      },
+      fontFamily: {
+        // to change, update font in _document.js
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
