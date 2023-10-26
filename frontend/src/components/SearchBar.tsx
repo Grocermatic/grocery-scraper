0b9d74345f6a356a8be15223eb49de985cfc62c0
Meng Lin <menglinmaker@gmail.com>
2023-10-26 18:47:03 +1100

    Replicate Google searchbar UX

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,38 +1,25 @@
-import {
-  For,
-  Show,
-  createSignal,
-  onCleanup,
-  onMount,
-  splitProps,
-} from "solid-js"
-import { SearchIcon } from "../svg/SearchIcon"
+import { For, Show, createSignal, onCleanup, onMount, splitProps } from 'solid-js'
+import { SearchIcon } from '../svg/SearchIcon'
 
 export const SearchBar = (props: any) => {
-  const [local, others] = splitProps(props, [
-    "id",
-    "placeholder",
-    "onChange",
-    "onInput",
-    "suggestions",
-  ])
-  const [searchQuery, setSearchQuery] = createSignal("")
+  const [local, _] = splitProps(props, ['id', 'placeholder', 'onChange', 'onInput', 'suggestions'])
+  const [searchQuery, setSearchQuery] = createSignal('')
+  const [typedInput, setTypedInput] = createSignal('')
   const [suggestions, setSuggestions] = createSignal([])
-  const [selectedId, setSelectedId] = createSignal(-1)
+  const [selectedId, setSelectedId] = createSignal(0)
   let listRef: HTMLUListElement | undefined
 
   const search = () => {
     // Allow other events to complete before searching
-    setTimeout(() => {
-      local.onChange(searchQuery())
-      setSuggestions([])
-      setSelectedId(-1)
-    }, 50)
+    local.onChange(searchQuery())
+    setSuggestions([])
+    setSelectedId(0)
   }
 
   const input = (e: any) => {
     const query = e.target.value
     setSearchQuery(query)
+    setTypedInput(query)
     local.onInput(query)
     setSuggestions(local.suggestions)
   }
@@ -42,41 +29,48 @@ export const SearchBar = (props: any) => {
     return ((n % m) + m) % m
   }
 
-  const selectListElement = (listRef: HTMLUListElement, diff: number) => {
-    // Update styles of selected
-    if (selectedId() != -1) {
-      const listItemElement = listRef.getElementsByTagName("li")[selectedId()]
-      listItemElement.classList.remove("bg-light")
+  const selectListElementToggle = (listElement: any, on: boolean) => {
+    if (on) {
+      selectListElementToggle(listElement, false)
+      setSelectedId(Number(listElement.value))
+      listElement.classList.add('bg-light')
+    } else if (listRef) {
+      for (const listItemElement of listRef.getElementsByTagName('li')) {
+        listItemElement.classList.remove('bg-light')
+      }
     }
-    const newSelectedId = mod(selectedId() + diff, suggestions().length)
-    const newListItemElement = listRef.getElementsByTagName("li")[newSelectedId]
-    newListItemElement.classList.add("bg-light")
+  }
 
-    setSearchQuery(newListItemElement.innerText)
+  const selectListElement = (listRef: HTMLUListElement, diff: number) => {
+    const newSelectedId = mod(selectedId() + diff, suggestions().length + 1)
+    const newListItemElement = listRef.getElementsByTagName('li')[newSelectedId]
+    selectListElementToggle(newListItemElement, true)
+    if (newSelectedId == 0) setSearchQuery(typedInput())
+    else setSearchQuery(newListItemElement.innerText)
     setSelectedId(newSelectedId)
   }
 
   const selectSuggestion = (e: KeyboardEvent) => {
     if (!listRef) return
-    if (suggestions().length == 0) return
-    if (e.code === "ArrowDown") selectListElement(listRef, 1)
-    else if (e.code === "ArrowUp") selectListElement(listRef, -1)
+    if (e.code === 'ArrowDown') selectListElement(listRef, 1)
+    else if (e.code === 'ArrowUp') selectListElement(listRef, -1)
+    else if (e.code === 'Enter') search()
   }
 
   onMount(() => {
-    document.addEventListener("keydown", selectSuggestion)
+    document.addEventListener('keydown', selectSuggestion)
   })
 
   onCleanup(() => {
-    document.removeEventListener("keydown", selectSuggestion)
+    document.removeEventListener('keydown', selectSuggestion)
   })
 
   return (
     <>
       <ul ref={listRef} class="card overflow-clip !border-dark !border-[3px]">
-        <div class="flex">
+        <li class="flex bg-white">
           <input
-            onChange={search}
+            onSubmit={search}
             onInput={input}
             value={searchQuery()}
             id={local.id}
@@ -84,22 +78,28 @@ export const SearchBar = (props: any) => {
             type="text"
             autocorrect="on"
             autocomplete="off"
-            class="w-full px-4 py-2 bg-transparent focus:border-transparent focus:outline-none focus:ring-0"
+            class="w-full px-4 py-2 focus:border-transparent focus:outline-none"
           />
           <button aria-label="Search product" onClick={search} class="w-10">
             <SearchIcon class="m-auto h-5" />
           </button>
-        </div>
+        </li>
         <Show when={local.suggestions.length > 0}>
           <For each={suggestions()}>
-            {(suggestion, _) => (
+            {(suggestion, id) => (
               <>
-                <div class="h-px mx-2 bg-light" />
                 <li
-                  onclick={() => setSearchQuery(suggestion)}
-                  class="flex px-4 py-2 hover:bg-light hover hover:border-light"
+                  value={id().toString()}
+                  onMouseEnter={e => selectListElementToggle(e.target, true)}
+                  onMouseLeave={e => selectListElementToggle(e.target, false)}
+                  onclick={() => {
+                    setSearchQuery(suggestion)
+                    search()
+                  }}
+                  class="flex flex-col"
                 >
-                  {suggestion}
+                  <div class="h-px mx-3 bg-light" />
+                  <p class="px-4 py-2">{suggestion}</p>
                 </li>
               </>
             )}
