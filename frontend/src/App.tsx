14180ed84fe19203c8b5cefe921511c698f7d2b0
Meng Lin <menglinmaker@gmail.com>
2023-10-29 13:02:46 +1100

    Move snap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,8 +40,8 @@ export const App = () => {
         class="flex animate-none h-full flex-col p-2 overflow-y-auto snap-mandatory snap-both no-scrollbar"
       >
         <For each={searchResults()}>{(productInfo, _) => <ProductCard {...productInfo} />}</For>
-        <div class="h-2 shrink-0"></div>
-        <div class="card flex-shrink-0 grid place-content-center snap-start w-full h-full">
+        <div class="h-2 shrink-0 snap-start"></div>
+        <div class="card flex-shrink-0 grid place-content-center w-full h-full">
           <h1 class="text-center font-bold text-2xl">No more results</h1>
         </div>
         <div class="h-2 shrink-0"></div>
