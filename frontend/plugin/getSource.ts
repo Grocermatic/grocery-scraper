e4ad05bf0f80bad1c897ba8a3899d6bd0bf6ff29
Meng Lin <menglinmaker@gmail.com>
2023-11-03 11:43:24 +1100

    Get source

---
+++ frontend/plugin/getSource.ts
@@ -0,0 +1,29 @@
+import { CheerioAPI, Element } from 'cheerio'
+import { minify } from './minify'
+
+export const getSource = async (
+  $: CheerioAPI,
+  element: Element,
+  bundle: any,
+  sourceType: string,
+) => {
+  let source
+  const attributeName = element.attribs.src ? 'src' : 'href'
+  const resourcePath = element.attribs[attributeName]
+  if (resourcePath?.startsWith('http')) {
+    // Load JS from URL.
+    source = await (await fetch(resourcePath)).text()
+  } else if (element.attribs.src || element.attribs.href) {
+    // Load local JS from bundle.
+    const resourcePathWithoutLeadingSlash = element.attribs[attributeName].slice(1)
+    const bundleItem = bundle[resourcePathWithoutLeadingSlash]
+    delete element.attribs[attributeName]
+    source = minify(bundleItem.code, sourceType)
+    $(element).text(source)
+  } else {
+    // Load inline CSS from Html
+    source = minify($(element).text(), sourceType)
+    $(element).text(source)
+  }
+  return source
+}
