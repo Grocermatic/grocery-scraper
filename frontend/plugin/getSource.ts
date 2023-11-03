513fe6da117d378999798bcb4db11782e8da8221
Meng Lin <menglinmaker@gmail.com>
2023-11-03 20:10:09 +1100

    Inline external source

--- frontend/plugin/getSource.ts
+++ frontend/plugin/getSource.ts
@@ -8,18 +8,23 @@ export const getSource = async (
   sourceType: string,
 ) => {
   let source
-  if (element.attribs.src || element.attribs.href) {
+  const attributeName = element.attribs.src ? 'src' : 'href'
+  const resourcePath = element.attribs[attributeName]
+  if (resourcePath?.startsWith('http')) {
+    // Load JS from URL.
+    source = await (await fetch(resourcePath)).text()
+  } else if (element.attribs.src || element.attribs.href) {
     // Inline local source from bundle.
-    const attributeName = element.attribs.src ? 'src' : 'href'
     const resourcePathWithoutLeadingSlash = element.attribs[attributeName].slice(1)
-    const bundleItem = bundle[resourcePathWithoutLeadingSlash]
-    delete element.attribs[attributeName]
-    source = minify(bundleItem.code, sourceType)
-    $(element).text(source)
+    source = bundle[resourcePathWithoutLeadingSlash].code
   } else {
     // Load inline source
-    source = minify($(element).text(), sourceType)
-    $(element).text(source)
+    source = $(element).text()
   }
+  delete element.attribs.src
+  delete element.attribs.href
+  delete element.attribs.crossorigin
+  source = minify(source, sourceType)
+  $(element).text(source)
   return source
 }
