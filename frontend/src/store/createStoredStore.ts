0fc9da7c5d029c81606720adb672a518b4e3c189
Meng Lin <menglinmaker@gmail.com>
2023-11-05 10:49:35 +1100

    Create localstorage store

---
+++ frontend/src/store/createStoredStore.ts
@@ -0,0 +1,18 @@
+import { SetStoreFunction, createStore } from 'solid-js/store'
+
+export const createStoredStore = (
+  key: string,
+  defaultValue: any,
+  storage: any = localStorage,
+): [proxy: any, setter: SetStoreFunction<any>] => {
+  const initialValue = storage.getItem(key) ? (JSON.parse(storage.getItem(key)) as Object) : defaultValue
+
+  const [value, setValue] = createStore(initialValue as any)
+
+  const setValueAndStore = ((arg: any) => {
+    setValue(arg)
+    storage.setItem(key, JSON.stringify(arg))
+  }) as typeof setValue
+
+  return [value, setValueAndStore]
+}
