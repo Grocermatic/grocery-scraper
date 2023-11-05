9271174a2c084739c52a39ccec33097cbf56e9d0
Meng Lin <menglinmaker@gmail.com>
2023-11-05 14:19:22 +1100

    External default

---
+++ frontend/src/store/default.ts
@@ -0,0 +1,14 @@
+export const storeSelection = [
+  {
+    name: 'Aldi',
+    active: true,
+  },
+  {
+    name: 'Coles',
+    active: true,
+  },
+  {
+    name: 'Woolworths',
+    active: true,
+  },
+]
