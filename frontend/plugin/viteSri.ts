9a9e85c9ed03aa922ebce5ecda39f43f3d33115a
Meng Lin <menglinmaker@gmail.com>
2023-11-06 15:15:01 +1100

    Unsafe-inline script to view Google AdSense

--- frontend/plugin/viteSri.ts
+++ frontend/plugin/viteSri.ts
@@ -36,7 +36,7 @@ export const viteSri = (origins: string[]) => {
       contentSecurityPolicy += `connect-src ${origins.join(' ')};`
       contentSecurityPolicy += `font-src ${origins.join(' ')};`
       contentSecurityPolicy += await scripts.asyncForEach(`script-src-elem ${origins.join(' ')}`)
-      contentSecurityPolicy += await stylesheets.asyncForEach(`style-src 'strict-dynamic'`)
+      contentSecurityPolicy += await stylesheets.asyncForEach(`style-src 'unsafe-inline'`)
 
       const cspElement = $('meta').filter('[http-equiv=Content-Security-Policy]')[0]
       cspElement.attribs['content'] = contentSecurityPolicy
