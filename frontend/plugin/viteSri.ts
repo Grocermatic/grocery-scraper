1683458eb0bf66cfe2c178167b04b86068af5602
Meng Lin <menglinmaker@gmail.com>
2023-11-06 15:29:21 +1100

    Please make AdSense Preview work

--- frontend/plugin/viteSri.ts
+++ frontend/plugin/viteSri.ts
@@ -36,7 +36,7 @@ export const viteSri = (origins: string[]) => {
       contentSecurityPolicy += `connect-src ${origins.join(' ')};`
       contentSecurityPolicy += `font-src ${origins.join(' ')};`
       contentSecurityPolicy += await scripts.asyncForEach(`script-src-elem ${origins.join(' ')}`)
-      contentSecurityPolicy += `style-src https:;`
+      contentSecurityPolicy += `style-src about: * data: blob: 'unsafe-inline';`
       await stylesheets.asyncForEach(`style-src 'unsafe-inline'`)
 
       const cspElement = $('meta').filter('[http-equiv=Content-Security-Policy]')[0]
