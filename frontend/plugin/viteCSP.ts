959761cd1f005f28f90f14cc14660c8a3dcf7ce8
Meng Lin <menglinmaker@gmail.com>
2023-11-06 23:15:20 +1100

    Unsafe-inline css

--- frontend/plugin/viteCSP.ts
+++ frontend/plugin/viteCSP.ts
@@ -73,7 +73,8 @@ export const viteCSP = (configCsp: ConfigCsp = {}) => {
 
       // Implement SRI for scripts and stylesheets.
       cspLine += await $('script').asyncForEach(`script-src ${joinMainSrc(configCsp.scriptSrc)}`)
-      cspLine += await $('style').asyncForEach(`style-src ${joinMainSrc(configCsp.styleSrc)}`)
+      await $('style').asyncForEach(`style-src ${joinMainSrc(configCsp.styleSrc)}`)
+      cspLine += `style-src 'unsafe-inline';`
 
       // Generate _headers file
       generateHeadersFile(cspLine)
