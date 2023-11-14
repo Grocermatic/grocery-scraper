831127bbfc1e8dfa6bdf3c3b52a1de7727665fbe
Meng Lin <menglinmaker@gmail.com>
2023-11-14 17:12:47 +1100

    Create urlsafe hash

---
+++ common/safeSha256.ts
@@ -0,0 +1,9 @@
+import { createHash } from "crypto"
+
+export const safeSha256 = (input: string) => {
+  return createHash('sha256')
+    .update(input)
+    .digest('base64')
+    .replaceAll('/', '_')
+    .replaceAll('+', '-')
+}
