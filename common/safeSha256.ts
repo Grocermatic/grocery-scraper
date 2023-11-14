7b76c32b5883ade86b1cd66312c880a3634e5ebf
Meng Lin <menglinmaker@gmail.com>
2023-11-14 21:18:26 +1100

    Pretty

--- common/safeSha256.ts
+++ common/safeSha256.ts
@@ -1,4 +1,4 @@
-export const safeSha256 = async(input: string) => {
+export const safeSha256 = async (input: string) => {
   const encodedText = new TextEncoder().encode(input)
   const buffer = await crypto.subtle.digest('SHA-256', encodedText)
   const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer)))
