7684aeb754b7e2b4495de6120ce2055487cbfc8b
Meng Lin <menglinmaker@gmail.com>
2023-11-15 00:29:20 +1100

    Infinite scroll

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,10 +1,20 @@
-import { For, createEffect, createSignal } from 'solid-js'
+import { For, createEffect, createMemo, createSignal, onCleanup, onMount } from 'solid-js'
 import { ProductCard } from './molecules/productList/ProductCard'
 import { SearchFilter } from './molecules/filter/SearchFilter'
 import { AdCard } from './components/AdCard'
 
+function makeArr(startValue, stopValue, cardinality) {
+  var arr = [];
+  var step = (stopValue - startValue) / (cardinality - 1);
+  for (var i = 0; i < cardinality; i++) {
+    arr.push(startValue + (step * i));
+  }
+  return arr;
+}
+
 export const Search = () => {
   const [searchResults, setSearchResults] = createSignal<any[]>([])
+  const searchLength = createMemo(() => searchResults().length)
 
   let productListref: HTMLDivElement | undefined
   createEffect(() => {
@@ -12,24 +22,41 @@ export const Search = () => {
     productListref?.scrollTo(0, 0)
   })
 
+  const [visibleLength, setVisibleLength] = createSignal<number>(10)
+  const visibleResults = createMemo(() => searchResults().slice(0, visibleLength()))
+  let intersectionRef: HTMLDivElement | undefined
+  onMount(() => {
+    let options = {
+      rootMargin: "0px",
+      threshold: makeArr(0, 1, 100),
+    }
+    const observer = new IntersectionObserver((e) => {
+      setVisibleLength(visibleLength() + 1)
+      if (searchLength() == visibleLength()) observer.unobserve(intersectionRef)
+    }, options)
+    if (intersectionRef) observer.observe(intersectionRef)
+
+    onCleanup(() => {
+      if (intersectionRef) observer.unobserve(intersectionRef)
+    })
+  })
+
   return (
     <div class="h-full flex flex-col">
       <SearchFilter setSearchResults={setSearchResults} />
       <section
         ref={productListref}
-        class="flex animate-none h-full flex-col p-2 overflow-y-auto snap-mandatory snap-both no-scrollbar"
+        class="flex animate-none h-full flex-col p-2 overflow-y-auto no-scrollbar"
       >
-        <div class="h-2 shrink-0 snap-start"></div>
         <AdCard class="shrink-0" />
 
-        <For each={searchResults()}>{(productInfo, _) => <ProductCard {...productInfo} />}</For>
+        <For each={visibleResults()}>{(productInfo, _) => <ProductCard {...productInfo} />}</For>
         <div class="h-2 shrink-0 snap-start"></div>
-
-        <div class="card flex-shrink-0 grid place-content-center w-full h-full">
+        
+        <div ref={intersectionRef} class="card flex-shrink-0 grid place-content-center w-full h-full">
           <h1 class="text-center font-bold text-2xl">No more results</h1>
         </div>
 
-        <div class="h-2 shrink-0"></div>
       </section>
     </div>
   )
