fd61192d1ae5c66d9f3ef816495bbd6c4d27eca5
Meng Lin <menglinmaker@gmail.com>
2023-11-15 22:55:44 +1100

    Remove google ads

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,20 @@
 import { render } from 'solid-js/web'
 import { Route, Router, Routes } from '@solidjs/router'
-import { Search } from './Search'
+import { Search } from './pages/Search'
+/*
 import { createEffect } from 'solid-js'
 import { loadExternalJs } from './logic/externalJs'
 import { miniSearchLoaded } from './store/search'
+createEffect(() => {
+  if (miniSearchLoaded()) {
+    loadExternalJs(
+      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6950110112582359',
+    )
+  }
+})
+//*/
 
 const App = () => {
-  createEffect(() => {
-    if (miniSearchLoaded()) {
-      loadExternalJs(
-        'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6950110112582359',
-      )
-    }
-  })
-
   // Temporary redirect all to search page
   if (window.location.pathname != '/search') window.location.pathname = '/search'
 
