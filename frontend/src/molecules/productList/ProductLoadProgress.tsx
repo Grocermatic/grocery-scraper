adc6fba050e09369d0c8326bc6109637f2a8a9d4
Meng Lin <menglinmaker@gmail.com>
2023-11-16 20:45:50 +1100

    Embed ad into loader

--- frontend/src/molecules/productList/ProductLoadProgress.tsx
+++ frontend/src/molecules/productList/ProductLoadProgress.tsx
@@ -1,5 +1,6 @@
-import { createEffect, createSignal, splitProps } from 'solid-js'
+import { Show, createEffect, createSignal, splitProps } from 'solid-js'
 import { miniSearchLoaded } from '../../store/search'
+import { FaviconLogo } from '../../svg/FaviconLogo'
 
 export const ProductLoadProgress = (props: any) => {
   const [local, _] = splitProps(props, ['class'])
@@ -9,28 +10,46 @@ export const ProductLoadProgress = (props: any) => {
   createEffect(() => {
     console.log(progressPercentage())
     if (miniSearchLoaded() == 1) {
-      setTimeout(() => setHidden(true), 1000)
+      setTimeout(() => setHidden(true), 500)
     }
   })
 
   return (
-    <div
-      class={`card flex flex-col p-4 gap-2 overflow-hidden ${local.class} ${
-        hidden() ? 'hidden' : ''
-      }`}
-    >
-      <h3 class="text-base w-full font-bold">Loading products...</h3>
-      <span
-        class="block h-full w-full rounded-full bg-light overflow-hidden"
-        role="progressbar"
-        aria-labelledby="Product loading progress"
-        aria-valuenow={progressPercentage()}
+    <div class={`card aspect-[320/100] overflow-hidden ${local.class}`}>
+      <Show
+        when={hidden()}
+        fallback={
+          <span class="flex flex-col h-full p-4 gap-2 justify-center m-auto">
+            <h3 class="w-full font-bold">Loading products...</h3>
+            <span
+              class="block w-full rounded-full bg-light overflow-hidden"
+              role="progressbar"
+              aria-labelledby="Product loading progress"
+              aria-valuenow={progressPercentage()}
+            >
+              <span
+                class="block h-2 animate-pulse transition-all duration-300 rounded-full bg-dark"
+                style={`width: ${progressPercentage()}%`}
+              ></span>
+            </span>
+          </span>
+        }
       >
-        <span
-          class="block h-2 animate-pulse transition-all duration-1000 rounded-full bg-dark"
-          style={`width: ${progressPercentage()}%`}
-        ></span>
-      </span>
+        <ins
+          class="h-full w-full block text-center"
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-6950110112582359"
+          data-ad-slot="4758549840"
+        >
+          <span class="flex h-full justify-center m-auto">
+            <FaviconLogo class="h-20 px-1 pr-2 my-auto aspect-square fill-light" />
+            <div class="flex pr-2">
+              <h1 class="my-auto font-bold text-4xl  text-light">Grocermatic</h1>
+            </div>
+          </span>
+        </ins>
+      </Show>
     </div>
   )
 }
