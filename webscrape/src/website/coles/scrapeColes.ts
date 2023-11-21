feaa8c6401d801979c9adb43103a2c17b8568f9e
Meng Lin <menglinmaker@gmail.com>
2023-11-22 01:15:48 +1100

    Add all Coles food categories

--- webscrape/src/website/coles/scrapeColes.ts
+++ webscrape/src/website/coles/scrapeColes.ts
@@ -6,31 +6,22 @@ export const scrapeColes = async (cookie?: string) => {
 
   // Page links with get request health star filters
   const sectionLinks = [
+    // meat-seafood
+    'https://www.coles.com.au/browse/meat-seafood',
     // fruit-vegetables
     'https://www.coles.com.au/browse/fruit-vegetables',
     // dairy-eggs-fridge
     'https://www.coles.com.au/browse/dairy-eggs-fridge',
     // bakery
     'https://www.coles.com.au/browse/bakery',
+    // pantry
+    'https://www.coles.com.au/browse/pantry',
     // deli
     'https://www.coles.com.au/browse/deli',
-    // pantry
-    'https://www.coles.com.au/browse/pantry/baking',
-    'https://www.coles.com.au/browse/pantry/breakfast',
-    'https://www.coles.com.au/browse/pantry/canned-food-soups-noodles',
-    'https://www.coles.com.au/browse/pantry/coffee',
-    'https://www.coles.com.au/browse/pantry/herbs-spices',
-    'https://www.coles.com.au/browse/pantry/international-foods',
-    'https://www.coles.com.au/browse/pantry/jams-honey-spreads',
-    'https://www.coles.com.au/browse/pantry/oils-vinegars',
-    'https://www.coles.com.au/browse/pantry/pasta-rice-legumes-grains',
-    'https://www.coles.com.au/browse/pantry/pickled-vegetables-condiments',
-    'https://www.coles.com.au/browse/pantry/sauces',
-    'https://www.coles.com.au/browse/pantry/stocks-gravy',
+    // drinks
+    'https://www.coles.com.au/browse/drinks',
     // frozen
     'https://www.coles.com.au/browse/frozen',
-    // meat-seafood
-    'https://www.coles.com.au/browse/meat-seafood',
   ]
 
   for (const sectionLink of sectionLinks) {
