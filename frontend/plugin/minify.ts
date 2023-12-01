34b0937b977496837d2455f2c1e50f419e97e10f
Meng Lin <menglinmaker@gmail.com>
2023-12-02 09:33:26 +1100

    Extreme minify css

--- frontend/plugin/minify.ts
+++ frontend/plugin/minify.ts
@@ -11,7 +11,10 @@ export const minifyCss = (sourceCss: string) => {
   return transformSync(sourceCss, {
     loader: 'css',
     minify: true,
-  }).code.replaceAll(/--.[a-z-]+: ;/gi, '') // Delete undefined css variables
+  })
+    .code.replaceAll(/--.[a-z-]+: ;/gi, '') // Delete undefined css variables
+    .replaceAll(/--[a-z-]+: }/gi, '}')
+    .replaceAll(/: /gi, ':') // Delete spaces
 }
 
 export const minify = (source: string, sourceType: string) => {
