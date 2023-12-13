63f011830f81da97cbffe442a1a662a2a5216a20
Meng Lin <menglinmaker@gmail.com>
2023-12-14 00:02:28 +1100

    Correct mergeStepChart function

--- frontend/src/logic/chart/mergeStepChart.ts
+++ frontend/src/logic/chart/mergeStepChart.ts
@@ -1,4 +1,38 @@
+import { keys } from '../keys'
+import { stepChart } from './stepChart'
+
 export const mergeStepChart = (lineCharts: number[][][]) => {
+  const pointHash: {
+    [x: number]: {
+      y: number
+      chartId: number
+    }[]
+  } = {}
+  lineCharts.map((points: number[][], chartId: number) => {
+    for (const point of points) {
+      const oldPoint = pointHash[point[0]]
+      const currentPoint = {
+        y: point[1],
+        chartId: chartId,
+      }
+      if (!oldPoint) pointHash[point[0]] = [currentPoint]
+      else pointHash[point[0]].push(currentPoint)
+    }
+  })
+
+  const currentY: {
+    [chartId: number]: number
+  } = {}
+  const mergeStep: number[][] = []
+  for (const x of keys(pointHash).toSorted((a, b) => Number(a) - Number(b))) {
+    for (const point of pointHash[x]) currentY[point.chartId] = point.y
+    const minValue = Math.min(...Object.values(currentY))
+    const mergeId = mergeStep.length - 1
+    if (mergeId == -1 || mergeStep[mergeId][1] != minValue) mergeStep.push([Number(x), minValue])
+  }
+  return stepChart(mergeStep)
+
+  /*
   // Sort all points by x coordinate, then y coordinate
   let points: number[][] = []
   lineCharts.forEach((lineChart: number[][]) => (points = [...points, ...lineChart]))
@@ -37,4 +71,5 @@ export const mergeStepChart = (lineCharts: number[][][]) => {
     lastY = batchPoints[i][1]
   }
   return mergedLineChart
+  */
 }
--- frontend/src/logic/chart/test/mergeStepChart.test.ts
+++ frontend/src/logic/chart/test/mergeStepChart.test.ts
@@ -8,31 +8,25 @@ test('Function: mergeStepChart - should merge step charts', async () => {
       [0, 2],
       [5, 2],
     ],
-    stepChart(
-      [
-        [-1, 3],
-        [1, 1],
-        [4, 3],
-      ],
-      5,
-    ),
+    stepChart([
+      [-1, 3],
+      [1, 1],
+      [4, 3],
+    ]),
     stepChart([
       [-2, 4],
       [2, 0],
       [3, 4],
     ]),
   ]
-  const expectedChart = stepChart(
-    [
-      [-2, 4],
-      [-1, 3],
-      [0, 2],
-      [1, 1],
-      [2, 0],
-      [3, 1],
-      [4, 2],
-    ],
-    5,
-  )
+  const expectedChart = stepChart([
+    [-2, 4],
+    [-1, 3],
+    [0, 2],
+    [1, 1],
+    [2, 0],
+    [3, 1],
+    [4, 2],
+  ])
   expect(mergeStepChart(charts)).toEqual(expectedChart)
 })
