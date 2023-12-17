4cb6b41fa818004d112ee571d75e99d9fcd58d8f
Meng Lin <menglinmaker@gmail.com>
2023-12-17 19:26:09 +1100

    Add product info page for IGA

---
+++ webscrape/src/website/iga/getProductInfoPage.ts
@@ -0,0 +1,13 @@
+import { ProductInfoReport } from '../ProductInfoReport'
+import { filterUncomparableProduct } from './filterUncomparableProduct'
+import { getProductInfo } from './getProductInfo'
+
+export const getProductInfoPage = (productJson: string) => {
+  const report = new ProductInfoReport()
+
+  const products = filterUncomparableProduct(productJson)
+  for (const product of products) {
+    report.recordProductInfo(getProductInfo, product)
+  }
+  return report
+}
