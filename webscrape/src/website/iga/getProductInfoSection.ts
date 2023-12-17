9689317a90e5cb6d8a683a8efb362f5250be8357
Meng Lin <menglinmaker@gmail.com>
2023-12-18 00:33:17 +1100

    Add IGA full scraping capability

---
+++ webscrape/src/website/iga/getProductInfoSection.ts
@@ -0,0 +1,26 @@
+import { getRequestJson } from '../../request/scrapeJson'
+import { wait } from '../../request/wait'
+import { ProductInfoReport } from '../ProductInfoReport'
+import { getProductInfoPage } from './getProductInfoPage'
+
+export const getProductInfoSection = async (sectionName: string, cookie?: string) => {
+  const report = new ProductInfoReport()
+  const storeCode = '17742'
+  const paginationSize = 100
+  const baseUrl = `https://www.igashop.com.au/api/storefront/stores/${storeCode}/categories/Drinks/search`
+
+  let totalProduct = Infinity
+  for (let skipProduct = 0; skipProduct < totalProduct; skipProduct += paginationSize) {
+    const productJson = await getRequestJson(
+      `${baseUrl}?take=${paginationSize}&skip=${skipProduct}`,
+    )
+    if (productJson == '') break
+    if (totalProduct == Infinity) totalProduct = JSON.parse(productJson).total
+    await wait(1500)
+
+    report.recordProductInfoPage(getProductInfoPage, productJson)
+    const numProducts = report.get().productInfo.length
+    console.debug(`IGA - ${sectionName} - ${numProducts} products`)
+  }
+  return report
+}
---
+++ webscrape/src/website/iga/scrapeIga.ts
@@ -0,0 +1,65 @@
+/* istanbul ignore file */
+
+import { ProductInfoReport } from '../ProductInfoReport'
+import { getProductInfoSection } from './getProductInfoSection'
+
+export const scrapeIga = async (cookie?: string) => {
+  const report = new ProductInfoReport()
+
+  // Page links with get request health star filters
+  const sectionNames: string[] = [
+    // fruit-and-vegetable
+    'Vegetables',
+    'Fruit',
+    'Salads',
+    // pantry
+    'International Foods',
+    'Canned Food and Instant Meals',
+    'Rice, Pasta and Grains',
+    'Desserts and Toppings',
+    'Jam, Honey and Spreads',
+    'Confectionary',
+    'Snacks',
+    'Health Food',
+    'Baking',
+    'Condiments and Dressings',
+    'Cooking Sauces',
+    'Breakfast Foods',
+    'Soups',
+    // meat-seafood-and-deli
+    'Deli Specialties',
+    'Meat',
+    'Deli',
+    'Seafood',
+    // dairy-eggs-and-fridge
+    'Chilled and Fresh',
+    'Milk and Cream',
+    'Yoghurt',
+    'Eggs, Butter and Margarine',
+    'Vegetarian and Vegan',
+    'Cheese',
+    'Cream, Custards and Desserts',
+    // bakery
+    'Bakery',
+    // drinks
+    'Drinks',
+    // frozen
+    'Frozen Meals',
+    'Frozen Pies and Party Food',
+    'Ice Cream',
+    'Frozen Vegetables',
+    'Chips and Wedges',
+    'Frozen Fish and Seafood',
+    'Frozen Desserts',
+    'Frozen Meat',
+    'Frozen Vegetarian and Vegan Food',
+    'Frozen Fruit',
+    'Frozen Pizza',
+    'Ice',
+  ].map((category: string) => category.replaceAll(' ', '_').replaceAll(',', ''))
+
+  for (const sectionName of sectionNames) {
+    await report.recordProductInfoSection(getProductInfoSection, sectionName, cookie)
+  }
+  return report.removeDuplicate().sortProductInfoUnitPrice().recordScrapeSecond()
+}
