5b6e046cc55ccabe0877ce1f1c84b890fa1cc16f
Meng Lin <menglinmaker@gmail.com>
2024-01-05 10:38:38 +1100

    Export all social media SVG

---
+++ frontend/static/svg/index.ts
@@ -0,0 +1,29 @@
+import Favicon from './Favicon.astro'
+import FacebookIcon from './FacebookIcon.astro'
+import FlickrIcon from './FlickrIcon.astro'
+import GithubIcon from './GithubIcon.astro'
+import GooglePlusIcon from './GooglePlusIcon.astro'
+import InstagramIcon from './InstagramIcon.astro'
+import LastFmIcon from './LastFmIcon.astro'
+import LinkedinIcon from './LinkedinIcon.astro'
+import PinterestIcon from './PinterestIcon.astro'
+import Tumblericon from './Tumblericon.astro'
+import TwitterIcon from './TwitterIcon.astro'
+import VimeoIcon from './VimeoIcon.astro'
+import YoutubeIcon from './YoutubeIcon.astro'
+
+export {
+  Favicon,
+  FacebookIcon,
+  FlickrIcon,
+  GithubIcon,
+  GooglePlusIcon,
+  InstagramIcon,
+  LastFmIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  Tumblericon,
+  TwitterIcon,
+  VimeoIcon,
+  YoutubeIcon
+}
