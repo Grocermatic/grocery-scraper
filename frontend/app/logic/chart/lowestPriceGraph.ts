392fa7474620ea090ab45ece120319efa316e787
Meng Lin <menglinmaker@gmail.com>
2024-01-17 01:14:38 +1100

    Prettier

--- frontend/app/logic/chart/lowestPriceGraph.ts
+++ frontend/app/logic/chart/lowestPriceGraph.ts
@@ -1,8 +1,8 @@
-import { daySinceEpoch } from "../../../../common/daysSinceEpoch"
-import type { Coordinates, ProductInfoPublic, ProductPriceDay } from "../../../../common/interface"
-import { roundDecimal } from "../../../../common/roundDecimal"
-import { mergeStepChart } from "./mergeStepChart"
-import { transpose } from "./transpose"
+import { daySinceEpoch } from '../../../../common/daysSinceEpoch'
+import type { Coordinates, ProductInfoPublic, ProductPriceDay } from '../../../../common/interface'
+import { roundDecimal } from '../../../../common/roundDecimal'
+import { mergeStepChart } from './mergeStepChart'
+import { transpose } from './transpose'
 
 export const lowestPriceGraph = (productList: ProductInfoPublic[]) => {
   const priceGraphs: Coordinates[][] = productList.map((product) =>
