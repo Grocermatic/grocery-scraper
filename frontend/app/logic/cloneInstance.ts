f537efc688a21d6afca03eff5f16e2dcab1940b9
Meng Lin <menglinmaker@gmail.com>
2024-01-16 19:12:38 +1100

    Add type to cloneInstance

--- frontend/app/logic/cloneInstance.ts
+++ frontend/app/logic/cloneInstance.ts
@@ -1,4 +1,4 @@
-export const cloneInstance = (instance: any) => {
+export const cloneInstance = <T>(instance: T) => {
   const instancePrototype = Object.getPrototypeOf(instance)
   const instanceTarget = Object.create(instancePrototype)
   return Object.assign(instanceTarget, instance)
