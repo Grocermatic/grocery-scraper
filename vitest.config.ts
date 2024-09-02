277a7f8488d67254332286c52ad4b03ccb89b029
MengLinMaker <menglinmaker@gmail.com>
2024-09-02 20:49:11 +1000

    ci: increase testing speed

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude],
     coverage: {
+      enabled: true,
       include: ['common', 'webscrape'],
     },
+    // Faster than 'fork' pool at the cost of less isolation.
+    pool: 'threads',
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['verbose', 'github-actions']
+      : ['verbose'],
+    logHeapUsage: true,
   },
 })
